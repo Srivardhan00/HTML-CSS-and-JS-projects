d8ffd11a25d58e08e627e6768976b207b61c1bfe
Srivardhan00 <srivardhanveeramalli@gmail.com>
2024-03-25 00:02:14 +0530

    Updated

--- Project-1-Expanding-Cards/index.js
+++ Project-1-Expanding-Cards/index.js
@@ -1,29 +1,13 @@
-<<<<<<< HEAD
 cards = document.querySelectorAll(".card");
-cards.forEach(card=> {
-  card.addEventListener('click', () => {
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
     removeActiveClasses();
-    card.classList.add('active');
+    card.classList.add("active");
   });
 });
 
 function removeActiveClasses() {
   cards.forEach((card) => {
-    card.classList.remove('active');
+    card.classList.remove("active");
   });
 }
-=======
-cards = document.querySelectorAll(".card");
-cards.forEach(card=> {
-  card.addEventListener('click', () => {
-    removeActiveClasses();
-    card.classList.add('active');
-  });
-});
-
-function removeActiveClasses() {
-  cards.forEach((card) => {
-    card.classList.remove('active');
-  });
-}
->>>>>>> 04f8e13ab0ecbe0c2e34332cc09e7ac3166b92d8
