d26047939a50b0b3a7c04c5b06d4ac05e561b12a
Srivardhan00 <srivardhanveeramalli@gmail.com>
2024-03-25 20:34:52 +0530

    Add Second Project

---
+++ Project-2-Progress-Bar/index.js
@@ -0,0 +1,45 @@
+const circles = document.querySelectorAll('.circle')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const progress = document.getElementById('progress')
+
+let currentActive =1;
+
+next.addEventListener('click',()=>{
+    currentActive++;
+    if(currentActive>circles.length){
+        currentActive=circles.length
+    }
+    updateProgress()
+})
+
+prev.addEventListener('click',()=>{
+    currentActive--;
+    if(currentActive<1){
+        currentActive=1
+    }
+    updateProgress()
+})
+function updateProgress(){
+    console.log(currentActive);
+    circles.forEach((circle,idx)=>{
+        if(idx<currentActive){
+            circle.classList.add('active')
+        }
+        else{
+            circle.classList.remove('active')
+        }
+        progress.style.width=((currentActive-1)/(circles.length-1))*100+'%'
+        if(currentActive===circles.length){
+            next.disabled=true
+        }
+        else if(currentActive===1)
+        {
+            prev.disabled=true
+        }
+        else{
+            prev.disabled=false
+            next.disabled=false
+        }
+    })
+}
